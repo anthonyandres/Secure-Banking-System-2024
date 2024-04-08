ef484bd37c4328c728b0ece61083f01df29a1759
anthonyandres <anthonyandres1881@gmail.com>
2024-04-08 14:03:40 -0400

    Encryption method (DES)

---
+++ src/DES.java
@@ -0,0 +1,25 @@
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+
+public class DES {
+    Cipher encryptCipher = Cipher.getInstance("DES");
+    Cipher decryptCipher = Cipher.getInstance("DES");
+
+    DES(SecretKey key) throws Exception {
+        this.encryptCipher.init(1, key);
+        this.decryptCipher.init(2, key);
+    }
+
+    public String encrypt(String toEncrypt) throws Exception {
+        byte[] utf8 = toEncrypt.getBytes("UTF8");
+        byte[] encrypted = this.encryptCipher.doFinal(utf8);
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String toDecrypt) throws Exception {
+        byte[] decrypt = Base64.getDecoder().decode(toDecrypt);
+        byte[] output = this.decryptCipher.doFinal(decrypt);
+        return new String(output, "UTF8");
+    }
+}
