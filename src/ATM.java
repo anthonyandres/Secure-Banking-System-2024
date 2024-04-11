4bb4710f059f84bc8c6d3903598913c4d0cde1fd
anthonyandres <anthonyandres1881@gmail.com>
2024-04-10 20:33:09 -0400

    master key distribution and authentication

--- src/ATM.java
+++ src/ATM.java
@@ -1,14 +1,18 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.net.*;
 import java.security.*;
+import java.util.Base64;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ATM {
     static int nonce = 1 + (int)(Math.random() * 99999.0);
-    static String atmNonce;
 
     //reads the number from tmp.txt, which represents the current ATM id
-    //inelegant solution to a simple file numbering problem..
+    //inelegant solution to a simple file numbering problem.. it only supports up to 10 instances of ATM
     private static int getATMID() throws IOException {
         FileReader fr = new FileReader("tmp.txt");
         BufferedReader bufferedReader = new BufferedReader(fr);
@@ -34,16 +38,16 @@ public class ATM {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         String hostName = "localhost";
         int portNumber = 4444;
-
+        String ATMID = Integer.toString(getATMID());
         //creating keypair
         KeyPair keyPair = generateKeyPair();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate(); //privateKey only used locally in one instance of ATM
+        PublicKey ATMPublicKey = keyPair.getPublic();
+        PrivateKey ATMPrivateKey = keyPair.getPrivate(); //privateKey only used locally in one instance of ATM
 
         //storing public keys in file
         System.out.println("creating publicKey file");
-        ObjectOutputStream publicStream = new ObjectOutputStream(new FileOutputStream("ATM" + getATMID() + "PublicKey.xx"));
-        publicStream.writeObject(publicKey);
+        ObjectOutputStream publicStream = new ObjectOutputStream(new FileOutputStream("ATM" + ATMID + "PublicKey.xx"));
+        publicStream.writeObject(ATMPublicKey);
         publicStream.close();
 
 
@@ -54,20 +58,63 @@ public class ATM {
         ){
             //getting bank public key from file
             ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("BankPublicKey.xx"));
-            PublicKey bankKey = (PublicKey) inputStream.readObject();
-            System.out.println("THIS IS KDC'S PUBLIC KEY: " + bankKey.toString() + "\n");
+            PublicKey bankPublicKey = (PublicKey) inputStream.readObject();
+            //System.out.println("THIS IS BANK'S PUBLIC KEY: " + bankKey.toString() + "\n");
             inputStream.close();
 
             //creating rsa class for encryption/decryption
             RSA rsa = new RSA();
 
+            //telling bank server what public key file to read;
+            output.println(ATMID);
+
             //begin master key distribution/authentication protocol here:
+            //receive bank nonce and bank id, decrypt with ATM private key
+            String toDecryptBankNonce = input.readLine();
+            String toDecryptBankID = input.readLine();
+
+            String decryptedBankNonce = rsa.privateDecrypt(toDecryptBankNonce, ATMPrivateKey);
+            String decryptedBankID = rsa.privateDecrypt(toDecryptBankID, ATMPrivateKey);
+            System.out.println("\n||Received Bank Nonce: " + decryptedBankNonce + "||\nencrypted="+toDecryptBankNonce);
+            System.out.println("||Received ID: " + decryptedBankID + "||\nencrypted="+toDecryptBankID);
+
+
+            //Send bank nonce and ATM nonce, encrypted with bank public key
+            String ATMNonce = Integer.toString(nonce);
+            String encryptedBankNonce = rsa.publicEncrypt(decryptedBankNonce, bankPublicKey);
+            String encryptedATMNonce = rsa.publicEncrypt(ATMNonce, bankPublicKey);
+
+            output.println(encryptedBankNonce);
+            output.println(encryptedATMNonce);
+            System.out.println("\n||Sending Bank Nonce: " + decryptedBankNonce + "||\nencrypted="+encryptedBankNonce);
+            System.out.println("||Sending ATM Nonce: " + ATMNonce + "||\nencrypted="+encryptedATMNonce);
+
+            //Receive ATM Nonce, encrypted with ATM public key
+            String toDecryptATMNonce = input.readLine();
+            String decryptedATMNonce = rsa.privateDecrypt(toDecryptATMNonce, ATMPrivateKey);
+            System.out.println("\n||Received ATM Nonce: " + decryptedATMNonce + "||\nencrypted="+toDecryptATMNonce);
+
+
+            //Receive Master Key, decrypt with ATM private key, then decrypt with bank public key
+            int index = 0;
+            String incoming;
+            String[] doubleEncrypt = new String[3];
+            while(!Objects.equals(incoming = input.readLine(), "stop")){
+                doubleEncrypt[index] = incoming;
+                index++;
+            }
+
+            String decryptedFirstHalf = rsa.privateDecrypt(doubleEncrypt[0], ATMPrivateKey);
+            String decryptedSecondHalf = rsa.privateDecrypt(doubleEncrypt[1], ATMPrivateKey);
+            String combined = decryptedFirstHalf.concat(decryptedSecondHalf);
+            String masterKeyString = rsa.publicDecrypt(combined, bankPublicKey);
+            //recreating master key
+            byte[] decodedMasterKey = Base64.getDecoder().decode(masterKeyString);
+            SecretKey masterKey = new SecretKeySpec(decodedMasterKey, 0, decodedMasterKey.length, "DES");
+            System.out.println("||Received Master Key from the Bank: " + masterKeyString+"||");
 
 
             //after authentication call GUI forms:
-
-
-
 
 
         } catch (UnknownHostException e) {
--- src/BankServerThread.java
+++ src/BankServerThread.java
@@ -1,11 +1,16 @@
+import javax.crypto.SecretKey;
+import javax.crypto.KeyGenerator;
 import java.net.*;
 import java.io.*;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Base64;
 
 public class BankServerThread extends Thread{
+    static int nonce = 1 + (int)(Math.random() * 99999.0);
     private Socket socket = null;
     private ArrayList<BankServerThread> threadList;
     private PrintWriter output;
@@ -25,11 +30,65 @@ public class BankServerThread extends Thread{
             //read private key file for this thread to get the private key
             ObjectInputStream bankinput = new ObjectInputStream(new FileInputStream("BankPrivateKey.xx"));
             PrivateKey bankPrivateKey = (PrivateKey) bankinput.readObject();
+            bankinput.close();
+
+            //read public key file for the ATM this thread is dealing with
+            String ATMID = input.readLine();
+            ObjectInputStream ATMinput = new ObjectInputStream(new FileInputStream("ATM" + ATMID + "PublicKey.xx"));
+            PublicKey ATMPublicKey = (PublicKey) ATMinput.readObject();
+            ATMinput.close();
+
             RSA rsa = new RSA();
 
             //begin master key distribution protocol here:
 
+            //send bank nonce and id of bank "bank", encrypted with public key of ATM
+            //System.out.println();
+            String bankNonce = Integer.toString(nonce);
+            String bankID = "bank";
 
+            String encryptedBankNonce = rsa.publicEncrypt(bankNonce, ATMPublicKey);
+            String encryptedBankID = rsa.publicEncrypt(bankID, ATMPublicKey);
+
+            output.println(encryptedBankNonce);
+            output.println(encryptedBankID);
+            System.out.println("\n||Sending Nonce: " + bankNonce + "||\nencrypted="+encryptedBankNonce);
+            System.out.println("||Sending ID: " + bankID + "||\nencrypted="+encryptedBankID);
+
+            //receive bank nonce and ATM nonce, both encrypted with bank private key
+            String bankNonceToDecrypt = input.readLine();
+            String atmNonceToDecrypt = input.readLine();
+
+            String decryptedBankNonce = rsa.privateDecrypt(bankNonceToDecrypt, bankPrivateKey);
+            String decryptedATMNonce = rsa.privateDecrypt(atmNonceToDecrypt, bankPrivateKey);
+
+            System.out.println("\n||Received Bank Nonce: " + decryptedBankNonce + "||\nencrypted="+bankNonceToDecrypt);
+            System.out.println("||Received ATM Nonce: " + decryptedATMNonce + "||\nencrypted="+atmNonceToDecrypt);
+
+
+            //send ATM nonce, encrypted with public key of ATM
+            String encryptedATMNonce = rsa.publicEncrypt(decryptedATMNonce, ATMPublicKey);
+
+            output.println(encryptedATMNonce);
+            System.out.println("\n||Sending Nonce: " + decryptedATMNonce + "||\nencrypted="+encryptedATMNonce);
+
+
+            //double encrypting master key with bankPrivateKey then ATMPublicKey
+            SecretKey masterKey = KeyGenerator.getInstance("DES").generateKey();
+            String masterKeyString = Base64.getEncoder().encodeToString(masterKey.getEncoded());
+            String innerEncrypt = rsa.privateEncrypt(masterKeyString, bankPrivateKey);
+            //splitting encryption in half for further encryption (this is done so that the size of the string to encrypt is not too big)
+            int middle = innerEncrypt.length()/2;
+            String[] half = {innerEncrypt.substring(0, middle), innerEncrypt.substring(middle)};
+            //encrypting the first half
+            String firstHalfEncrypt = rsa.publicEncrypt(half[0], ATMPublicKey);
+            String secondHalfEncrypt = rsa.publicEncrypt(half[1], ATMPublicKey);
+
+            //sending double encrypted symmetric key masterKey
+            System.out.println("\n||Sending Master Key to authenticated ATM: " + masterKeyString+"||");
+            output.println(firstHalfEncrypt);
+            output.println(secondHalfEncrypt);
+            output.println("stop");
 
 
 
